581b29e2e60aae6b27a040a71ce36169ca03f537
Iury da Guia Salino <iury.salino@MJV.net>
2022-10-18 19:07:40 -0300

    basic structure project

---
+++ src/main/java/com/github/iurysalino/uploadfile/UploadfileApplication.java
@@ -0,0 +1,11 @@
+package com.github.iurysalino.uploadfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadfileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UploadfileApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/iurysalino/uploadfile/UploadfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.iurysalino.uploadfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
